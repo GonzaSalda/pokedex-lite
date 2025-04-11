0c79c185b1d935990a8710fcad7a2e88182d214d
GonzaSalda <gonza.salda00@gmail.com>
2025-04-11 12:50:57 -0300

    Add styles to pokemon-list

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -13,6 +13,27 @@ import { CommonModule } from '@angular/common';
 export class PokemonListComponent {
   pokemons: any[] = [];
 
+  typeColors: { [key: string]: string } = {
+    fire: "bg-red-500",
+    water: "bg-blue-500",
+    grass: "bg-green-500",
+    electric: "bg-yellow-500",
+    psychic: "bg-purple-500",
+    ice: "bg-cyan-500",
+    fighting: "bg-orange-600",
+    ground: "bg-yellow-700",
+    flying: "bg-indigo-400",
+    poison: "bg-purple-700",
+    bug: "bg-lime-600",
+    rock: "bg-gray-700",
+    ghost: "bg-indigo-700",
+    dragon: "bg-violet-600",
+    dark: "bg-gray-800",
+    steel: "bg-gray-500",
+    fairy: "bg-pink-400",
+    normal: "bg-gray-400",
+  };
+
   constructor(
     private pokemonService: PokemonService,
     private authService: AuthService,
@@ -48,5 +69,9 @@ export class PokemonListComponent {
         error: (err) => console.error('Error fetching details:', err),
       });
     });
+  }
+
+  getTypeColor(type: string): string {
+    return this.typeColors[type] || "bg-gray-500";
   }
 }
