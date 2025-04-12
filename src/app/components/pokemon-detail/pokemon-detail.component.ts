878f645eb343583746c112aa2ece07d08145a2e4
GonzaSalda <gonza.salda00@gmail.com>
2025-04-12 00:31:25 -0300

    Refactor PokemonDetailComponent HTML and TypeScript

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -21,17 +21,37 @@ export class PokemonDetailComponent {
     private router: Router
   ) {}
 
+  typeColors: { [key: string]: string } = {
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    grass: 'bg-green-500',
+    electric: 'bg-yellow-500',
+    psychic: 'bg-purple-500',
+    ice: 'bg-cyan-500',
+    fighting: 'bg-orange-600',
+    ground: 'bg-yellow-700',
+    flying: 'bg-indigo-400',
+    poison: 'bg-purple-700',
+    bug: 'bg-lime-600',
+    rock: 'bg-gray-700',
+    ghost: 'bg-indigo-700',
+    dragon: 'bg-violet-600',
+    dark: 'bg-gray-800',
+    steel: 'bg-gray-500',
+    fairy: 'bg-pink-400',
+    normal: 'bg-gray-400',
+  };
+
   ngOnInit() {
     if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/']);
       return;
     }
- 
 
     const name = this.route.snapshot.paramMap.get('name');
     if (name) {
       this.loadPokemonDetails(name);
-      console.log(this.pokemon)
+      console.log(this.pokemon);
     }
   }
 
@@ -61,5 +81,9 @@ export class PokemonDetailComponent {
 
   editPokemon() {
     this.router.navigate(['/pokemon/edit', this.pokemon.name]);
+  }
+
+  getTypeColor(type: string): string {
+    return this.typeColors[type] || 'bg-gray-500';
   }
 }
